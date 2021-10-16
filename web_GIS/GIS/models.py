d3ffe1185ae231147086464817895db166162c7f
Taha <tahadavari1381@gmail.com>
2021-10-16 16:05:52 +0330

    create Layer model

--- web_GIS/GIS/models.py
+++ web_GIS/GIS/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Layer(models.Model):
+    layer_name = models.CharField(max_length=50)
+    workspace = models.CharField(max_length=50)
+    layer_alias = models.CharField(max_length=50)
+    server_address = models.URLField()
+    user_owner = models.ManyToManyField(User)
+
+    def __str__(self):
+        return f'{self.layer_alias} : {self.workspace} : {self.layer_name} : {self.server_address}'
+
